c480005c5b855490dcb0bbcc289a8cf1373e043b
pablo.echegaray <pablo.echegaray97@gmail.com>
2024-10-08 06:36:30 -0300

    push -6

--- src/components/screens/login/Login.tsx
+++ src/components/screens/login/Login.tsx
@@ -1,31 +1,46 @@
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
-
+import styles from './Login.module.css'
+import { FormEvent, useState } from 'react';
 export const Login = () => {
-    return (
-        <Form>
-            <div>
-                <span className="material-symbols-outlined">
-                    group
-                </span>
-            </div>
-            <Form.Group className="mb-3">
-                <Form.Label>Usuario</Form.Label>
-                <Form.Control name="user" type="text" placeholder="Usuario" />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control name="password" type="password" placeholder="Contraseña" />
-            </Form.Group>
-            <Form.Check
-                type="switch"
-                id="custom-switch"
-                label="Mostrar contraseña"
-            />
-            <div>
-                <Button variant="primary">Primary</Button>{' '}
-            </div>
+    const [showPass, setShowPass] = useState(false)
+    const handleSubmitForm = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
 
-        </Form>
+    }
+    return (
+        <div className={styles.containerLogin}>
+            <div className={styles.containerForm}>
+                <div>
+                    <span className="material-symbols-outlined" style={{ fontSize: '10vh' }}>
+                        group
+                    </span>
+                </div>
+                <Form onSubmit={handleSubmitForm}>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Usuario</Form.Label>
+                        <Form.Control name="user" type="text" placeholder="Usuario" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control name="password" type={showPass ? "text" : "password"} placeholder="Contraseña" />
+                    </Form.Group>
+                    <Form.Check
+                        onChange={() => {
+                            setShowPass(!showPass)
+                        }} 
+                        type="switch"
+                        id="custom-switch"
+                        label="Mostrar contraseña"
+
+                    />
+                    <div className='d-flex justify-content-center align-items-center mt-2'>
+                        <Button variant="primary">Primary</Button>{' '}
+                    </div>
+
+                </Form>
+            </div>
+        </div>
     )
 }
